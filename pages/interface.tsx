252a1047accb761fb9cc75e3e967dac907675472
-v <devontheroof@gmail.com>
2022-11-25 21:14:04 +0100

    interface but no state

--- pages/interface.tsx
+++ pages/interface.tsx
@@ -1,3 +1,16 @@
+import dynamic from "next/dynamic";
+
+const FirstInterface = dynamic(
+  () => import("../src/interface/interfacep5wrapper"),
+  {
+    ssr: false,
+  }
+);
+
 export default function Interface() {
-  return <div>Interface</div>;
+  return (
+    <div>
+      <FirstInterface />
+    </div>
+  );
 }
--- pages/showcase.tsx
+++ pages/showcase.tsx
@@ -1,8 +1,11 @@
 import dynamic from "next/dynamic";
 
-const FirstShowcase = dynamic(() => import("../animation/animationp5wrapper"), {
-  ssr: false,
-});
+const FirstShowcase = dynamic(
+  () => import("../src/animation/animationp5wrapper"),
+  {
+    ssr: false,
+  }
+);
 
 export default function Showcase() {
   return (
