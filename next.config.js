9deb8c970c319a51c0d4f90db907fd4ab8920386
-v <devontheroof@gmail.com>
2022-12-19 18:39:09 +0100

    alea from public

--- next.config.js
+++ next.config.js
@@ -2,6 +2,18 @@
 const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
-}
+  async rewrites() {
+    return [
+      {
+        source: "/alea",
+        destination: "/alea/index.html",
+      },
+      {
+        source: "/alea/interface",
+        destination: "/alea/interface/index.html",
+      },
+    ];
+  },
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
